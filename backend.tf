9d1371e74877385b10f146f75256d78a7260e7b1
amit <amitwabale1997@gmail.com>
2025-02-06 13:12:09 +0000

    Runs for backend setup

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "us-east-1-steppoc"
     region = "us-east-1"
-    key = "terraform/terraform.state"
+    key = "terraform/terraform.tfstate"
     #dynamodb_table = "terraform_lock_table"
   }
 }
